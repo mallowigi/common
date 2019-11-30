99ed4b72e3579f2c245514ba5ff9674c9008576e
mallowigi <heliosaian@gmail.com>
2019-11-30 13:47:07 +0200

    wip

---
+++ proto/users/option.ts
@@ -0,0 +1,11 @@
+import { ClientOptions, Transport } from '@nestjs/microservices';
+import { join }                     from 'path';
+
+export const usersGrpcClientOptions: ClientOptions = {
+  transport: Transport.GRPC,
+  options:   {
+    url:       'users:50050',
+    package:   'service',
+    protoPath: join(__dirname, './service.proto'),
+  },
+};
